32c11c146a19a1a2169cde067c875279313a8436
Jermyiah <31228460+jge162@users.noreply.github.com>
2023-02-04 02:17:51 -0800

    updated

--- Scrap_Latestnews.py
+++ Scrap_Latestnews.py
@@ -1,24 +1,39 @@
 import requests
-from bs4 import BeautifulSoup
 
-# specify the URL of the website containing the news
-url = 'https://www.example.com/news'
-
-# make an HTTP GET request to the website
+url = "https://data.cnn.com/breaking_news/domestic.json"
 response = requests.get(url)
 
-# parse the HTML content of the response
-soup = BeautifulSoup(response.text, 'html.parser')
+data = response.json()
 
-# find all the news articles on the page
-articles = soup.find_all('article')
+for article in data:
+    print(article)
 
-# extract the headline and summary of each article
-for article in articles:
-    headline = article.find('h2').text
-    summary = article.find('p').text
-    
-    # print the headline and summary
-    print('Headline:', headline)
-    print('Summary:', summary)
-    print('---')
+# specify the URL of the breaking news API
+url = 'https://data.cnn.com/breaking_news/domestic.json'
+
+# make an HTTP GET request to the API
+response = requests.get(url)
+
+# check the status code of the response to make sure the request was successful
+if response.status_code == 200:
+    # the request was successful, parse the JSON data
+    data = response.json()
+
+    print(data)  # debug: print the contents of data
+
+    # extract the headline and summary of each article
+    for article in data['articles']:
+        headline = article['headline']
+        summary = article['summary']
+
+        # print the headline and summary
+        print('Headline:', headline)
+        print('Summary:', summary)
+        print('---')
+else:
+    # the request was not successful, print an error message
+    print(f'Failed to retrieve breaking news, status code: {response.status_code}')
+
+"""
+sudo python3 /Users/home/Documents/GitHub/Python_Scripts/Scrap_Latestnews.py
+"""
