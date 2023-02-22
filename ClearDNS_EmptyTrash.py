4e456b74ceef07228481329a9f99786cee5f7ae4
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-22 02:48:15 -0800

    Update ClearDNS_EmptyTrash.py

--- ClearDNS_EmptyTrash.py
+++ ClearDNS_EmptyTrash.py
@@ -1,25 +1,20 @@
-import subprocess
 import os
+import subprocess
 
 
 def clear_dns_cache():
     try:
         subprocess.call(["dscacheutil", "-flushcache"])
-        print("====================================")
-        print("= DNS cache cleared: Successfully. =")
-        print("====================================")
+        print("DNS cache cleared: Successfully! ")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
 def empty_trash():
     try:
-        home = os.path.expanduser("~")
-        trash = os.path.join(home, ".Trash")
-        subprocess.call(["rm", "-rf", trash + "/*"])
-        print("====================================")
-        print("= Trash bin emptied: Successfully. =")
-        print("====================================")
+        # Run the osascript command to empty the trash bin
+        subprocess.call(['osascript', '-e', 'tell application "Finder" to empty trash'])
+        print("Trash bin has been emptied!")
 
     except Exception as e:
         print(f"An error occurred: {e}")
