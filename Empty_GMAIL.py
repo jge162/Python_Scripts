d0dd30bd03355ea4e70bb6194a45d099d399360b
Jermyiah <escobarjeremy@csu.fullerton.edu>
2023-02-24 00:53:46 -0800

    Create Empty_GMAIL.py

---
+++ Empty_GMAIL.py
@@ -0,0 +1,31 @@
+
+import imaplib
+
+# Connect to the Gmail IMAP server
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+
+# Login to your Gmail account
+mail.login('your_email@gmail.com', 'your_password')
+
+# List the email folders in your account
+typ, data = mail.list()
+for folder in data:
+    print(folder.decode())
+
+# Select the spam folder
+typ, data = mail.select('"[Gmail]/Spam"')
+
+# Search for all messages in the spam folder
+typ, data = mail.search(None, 'ALL')
+
+# Delete all messages in the spam folder
+if data:
+    for num in data[0].split():
+        mail.store(num, '+FLAGS', '\\Deleted')
+    mail.expunge()
+
+# Close the mailbox and log out
+mail.close()
+mail.logout()
+
+print('All messages from the spam folder have been Deleted Successfully.')
